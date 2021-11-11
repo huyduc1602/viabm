76e36eb48b564f089cc11b2045c442915bfb68cb
duchh <duchh@aht-asia.com>
2021-11-11 19:32:06 +0700

    Bỏ đã bán trang đăng nhập

--- public/client/Login.php
+++ public/client/Login.php
@@ -45,9 +45,9 @@
                                 <th><?=lang(77);?></th>
                                 <th width="10%" class="text-center"><?=lang(85);?></th>
                                 <th width="10%" class="text-center"><?=lang(78);?></th>
-                                <?php if($CMSNT->site('display_sold') == 'ON') { ?>
-                                <th width="10%" class="text-center"><?=lang(140);?></th>
-                                <?php }?>
+<!--                                --><?php //if($CMSNT->site('display_sold') == 'ON') { ?>
+<!--                                <th width="10%" class="text-center">--><?//=lang(140);?><!--</th>-->
+<!--                                --><?php //}?>
                                 <th width="10%" class="text-center"><?=lang(74);?></th>
                             </tr>
                         </thead>
@@ -71,11 +71,11 @@
                                     <img width="40px" src="<?=BASE_URL('template/flag/'.$row['quocgia'].'.svg');?>" />
                                     <?php }?>
                                 </td>
-                                <td class="text-center">
-                                    <b style="color: red;;">
-                                        <?=format_cash($conlai);?>
-                                    </b>
-                                </td>
+<!--                                <td class="text-center">-->
+<!--                                    <b style="color: red;;">-->
+<!--                                        --><?//=format_cash($conlai);?>
+<!--                                    </b>-->
+<!--                                </td>-->
                                 <?php if($CMSNT->site('display_sold') == 'ON') { ?>
                                 <td class="text-center">
                                     <b style="color: green;;">
