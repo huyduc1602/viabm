856b7849ae9f0fd14075f742b7b0059df15ef034
duchh <duchh@aht-asia.com>
2021-11-14 11:58:56 +0700

    Thêm chức năng bài viết 11h58

---
+++ public/admin/AddBlog.php
@@ -0,0 +1,262 @@
+<?php
+    require_once("../../config/config.php");
+    require_once("../../config/function.php");
+    $title = 'THÊM BÀI VIẾT | '.$CMSNT->site('tenweb');
+    require_once("../../public/admin/Header.php");
+    CheckLogin();
+    CheckAdmin();
+    require_once("../../public/admin/Sidebar.php");
+    require_once(__DIR__."/../../includes/checkLicense.php");
+?>
+<?php
+if(isset($_POST['btnSubmit']) && isset($_POST['name']) && isset($_POST['categoryId']) && isset($_POST['detail']) && $getUser['level'] == 'admin')
+{
+    if($getUser['level'] != 'admin')
+    {
+        admin_msg_error('Chức năng này chỉ dành cho Admin.', '', 2000);
+    }
+    if($CMSNT->site('status_demo') == 'ON')
+    {
+        admin_msg_error("Chức năng này không khả dụng trên trang web DEMO!", "", 2000);
+    }
+    if(!isset($_SESSION['username'])){
+        admin_msg_success("Vui lòng đăng nhập", BASE_URL('Auth/Login'), 1000);
+    }
+    $userId =  $CMSNT->getUser($_SESSION['username']);
+    $name = check_string($_POST['name']);
+    if(strlen($name) < 2 || strlen($name) > 255)
+    {
+        admin_msg_error("Vui lòng nhập tên từ 2 đến 255 ký tự", '', 1000);
+    }
+    $slug = check_string($_POST['slug']);
+    if(strlen($slug) < 2 || strlen($slug) > 255)
+    {
+        admin_msg_error("Vui lòng nhập slug từ 2 đến 255 ký tự", '', 1000);
+    }
+    if(!preg_match('/^[a-z][-a-z0-9]*$/', $slug)){
+        admin_msg_error("Slug không hợp lệ (bao gồm chữ, số và dấu gạch nối)", '', 1000);
+    }
+    $categoryId = $_POST['categoryId'];
+    if(!isset($categoryId))
+    {
+        admin_msg_error("Vui lòng chọn danh mục", '', 1000);
+    }
+    $imgUpload = uploadImage($_FILES["fileupload"],'/uploads/image/');
+    $detail = $_POST['detail'];
+    $sumary = check_string($_POST['sumary']);
+    $create = $CMSNT->insert("blog", array(
+        'name'      => $name,
+        'slug'        => $slug,
+        'image'           => $imgUpload,
+        'detail'    => $detail,
+        'sumary'          => $sumary,
+        'categoryId'           => $categoryId,
+        'userId'       => $userId,
+        'createdDate'   => gettime(),
+        'updatedDate'   => gettime()
+    ));
+    if($create)
+    {
+        admin_msg_success("Tạo bài viết thành công", BASE_URL('Admin/Blogs'), 1000);
+    }
+    else
+    {
+//        admin_msg_error("Không thể thêm bài viết, vui lòng thử lại sau.", "", 2000);
+    }
+}
+?>
+
+
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Thêm tin tức</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-outline card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">THÊM BÀI VIẾT MỚI</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                                    class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form action="" enctype="multipart/form-data" method="POST">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Tên bài viết (*)</label>
+                                <div class="col-sm-9">
+                                    <div class="form-line">
+                                        <textarea class="form-control h-150px" placeholder="VD: Hướng dẫn đăng nhập..." name="name" id="Name" rows="1"
+                                            required></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Slug(Đường dẫn) </label>
+                                <div class="col-sm-9">
+                                    <div class="form-line">
+                                        <input type="text" name="slug" id ="Slug" value="" placeholder="Ví dụ: huong-dan-dang-nhap"
+                                            class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Ảnh</label>
+                                <div class="col-sm-9">
+                                    <div class="form-line">
+                                        <input type="file" name="fileupload"
+                                            class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Danh mục</label>
+                                <div class="col-sm-9">
+                                    <select class="form-control" name="categoryId" required>
+                                        <option value="">* Chọn danh mục</option>
+                                        <?php foreach($CMSNT->get_list("SELECT * FROM `category_blog` WHERE id > 1") as $category) { ?>
+                                        <option value="<?=$category['id'];?>"><?=$category['name'];?></option>
+                                        <?php }?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Mô tả ngắn</label>
+                                <div class="col-sm-9">
+                                    <div class="form-line">
+                                        <textarea class="form-control h-150px"
+                                            placeholder="Mô tả ngắn về bài viết" name="sumary"
+                                            rows="6"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Nội dung</label>
+                                <div class="col-sm-9">
+                                    <div class="form-line">
+                                        <textarea class="textarea" name="detail"
+                                            rows="6">Nội dung  bài viết </textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" name="btnSubmit" class="btn btn-primary btn-block waves-effect">
+                                <span>THÊM NGAY</span>
+                            </button>
+                            <a type="button" href="<?=BASE_URL('Admin/Blogs');?>"
+                                class="btn btn-danger btn-block waves-effect">
+                                <span>TRỞ LẠI</span>
+                            </a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+<div class="modal fade" id="Listflag" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="defaultModalLabel">DANH SÁCH QUỐC GIA</h4>
+            </div>
+            <div class="modal-body">
+                <div class="table-responsive">
+                    <table id="datatable" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>FLAG</th>
+                                <th>FILE NAME</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = COUNT(dirToArray('../../template/flag/')); foreach(dirToArray('../../template/flag/') as $row){ ?>
+                            <?php $path_parts = pathinfo('../../template/flag/'.$row); ?>
+                            <tr>
+                                <td><?=$i--;?></td>
+                                <td><img width="40px" src="<?=BASE_URL('template/flag/'.$row);?>" />
+                                </td>
+                                <td><?=$path_parts['filename'];?></td>
+                            </tr>
+                            <?php }?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger waves-effect"
+                    data-dismiss="modal"><span>ĐÓNG</span></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+$(function() {
+    $("#datatable").DataTable({
+        "responsive": true,
+        "autoWidth": false,
+    });
+});
+</script>
+<script>
+$(function() {
+    // Summernote
+    $('.textarea').summernote()
+})
+</script>
+<script>
+    //Auto slug
+    $(function () {
+        $('#Name').keyup(function () {
+            $slug = to_slug($('#Name').val());
+            $('#Slug').val($slug);
+            console.log($(this).val())
+        });
+    });
+    function to_slug(str) {
+        // Chuyển hết sang chữ thường
+        str = str.toLowerCase();
+
+        // xóa dấu
+        str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+        str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+        str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
+        str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+        str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+        str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+        str = str.replace(/(đ)/g, 'd');
+
+        // Xóa ký tự đặc biệt
+        str = str.replace(/([^0-9a-z-\s])/g, '');
+
+        // Xóa khoảng trắng thay bằng ký tự -
+        str = str.replace(/(\s+)/g, '-');
+
+        // xóa phần dự - ở đầu
+        str = str.replace(/^-+/g, '');
+
+        // xóa phần dư - ở cuối
+        str = str.replace(/-+$/g, '');
+
+        // return
+        return str;
+    }
+</script>
+<?php 
+    require_once("../../public/admin/Footer.php");
+?>
---
+++ public/admin/Blogs.php
@@ -0,0 +1,144 @@
+<?php
+    require_once("../../config/config.php");
+    require_once("../../config/function.php");
+    CheckLogin();
+    CheckAdmin();
+    $title = 'DANH MỤC TÀI KHOẢN | '.$CMSNT->site('tenweb');
+    require_once("../../public/admin/Header.php");
+    require_once("../../public/admin/Sidebar.php");
+    require_once(__DIR__."/../../includes/checkLicense.php");
+    
+    if(isset($_POST['XoaBaiViet']) && $getUser['level'] == 'admin' )
+    {
+        $id = check_string($_POST['id']);
+        if(!$row = $CMSNT->get_row("SELECT * FROM `blog` WHERE `id` = '$id' "))
+        {
+            msg_error2("ID cần xóa không tồn tại trong hệ thống !");
+        }
+        if($CMSNT->site('status_demo') == 'ON')
+        {
+            admin_msg_error("Chức năng này không khả dụng trên trang web DEMO!", "", 2000);
+        }
+        // GHI LOG
+        $file = @fopen('../../logs/XoaBaiViet.txt', 'a');
+        if ($file)
+        if ($file)
+        {
+            $data = "[LOG] Baì viết ID ".$row['id']." đã bị xóa khỏi hệ thống vào lúc ".gettime().PHP_EOL;
+            fwrite($file, $data);
+            fclose($file);
+        }
+        $CMSNT->remove("blog", " `id` = '$id' ");
+        admin_msg_success("Xóa thành công !", "", 1000);
+    }
+
+?>
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Danh sách bài viết</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-outline card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">QUẢN LÝ BÀI VIẾT</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                                    class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                    <div id="thongbao"></div>
+                        <div class="table-responsive">
+                            <table id="datatable" class="table table-bordered table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>STT</th>
+                                        <th>TÊN</th>
+                                        <th>SLUG</th>
+                                        <th>DANH MỤC</th>
+                                        <th>NGƯỜI TẠO</th>
+                                        <th>THAO TÁC</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($CMSNT->get_list(" SELECT * FROM `blog` ORDER BY id DESC ") as $row){ ?>
+                                    <tr>
+                                        <td><?=$row['id'];?></td>
+                                        <td><b><?=$row['name'];?></b></td>
+                                        <td><?=$row['slug'];?></td>
+                                        <td><?=display_loai($row['categoryId']);?></td>
+                                        <td><?=$row['userId'];?></td>
+                                        <td>
+                                            <a type="button" href="<?=BASE_URL('Admin/Blog/Edit/');?><?=$row['id'];?>"
+                                                class="btn bg-black">
+                                                <span>EDIT</span></a>
+
+                                            <button class="btn btn-danger btnDelete" id="XoaBaiViet" data-id="<?=$row['id'];?>"><i
+                                                    class="fas fa-trash"></i>
+                                                <span>DELETE</span>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <?php }?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<script type="text/javascript">
+$(".btnDelete").on("click", function() {
+    Swal.fire({
+        title: 'Xác nhận xóa bài viết',
+        text: "Bạn có chắc chắn xóa bài viết này không ?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'XÓA NGAY',
+        cancelButtonText: 'HỦY'
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            $.ajax({
+                url: "<?=BASE_URL("public/admin/Blogs.php");?>",
+                method: "POST",
+                data: {
+                    XoaBaiViet: true,
+                    id: $(this).attr("data-id")
+                },
+                success: function(response) {
+                    $("#thongbao").html(response);
+                }
+            });
+        }
+    })
+});
+</script>
+<script>
+$(function() {
+    $("#datatable").DataTable({
+        "responsive": true,
+        "autoWidth": false,
+    });
+});
+</script>
+
+<?php 
+    require_once("../../public/admin/Footer.php");
+?>
